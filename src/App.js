ce6bd9594024ccbeb50ae61dd11d1751569766b3
Ayush Baranwal <abaran803@gmail.com>
2021-08-29 17:01:53 +0530

    Change BrowserRouter to HashRouter to make routing working on git pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
 import AllRoutes from "./AllContents/AllRoutes";
 import Footer from "./HomePage/Footer";
 import Header from "./HomePage/Header";
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Header />
-        <AllRoutes />
-        <Footer />
-      </div>
-    </Router>
+    <div>
+      <Header />
+      <AllRoutes />
+      <Footer />
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { HashRouter as Router } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./ReduxComponents/store";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
