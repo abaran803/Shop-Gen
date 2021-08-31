5e9bb3a52697adfe1d3f925f970656809c45f091
Ayush Baranwal <abaran803@gmail.com>
2021-08-31 10:11:46 +0530

    Updates

---
+++ static/js/9.4f067e30.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpe-commerce"]=this["webpackJsonpe-commerce"]||[]).push([[9],{45:function(e,c,s){"use strict";s.r(c);var t=s(0);c.default=function(e){return Object(t.jsx)("div",{className:"col-md-4 col-12",children:Object(t.jsx)("div",{className:"card bg-dark text-white",children:Object(t.jsx)("a",{href:"category.html",children:Object(t.jsx)("img",{className:"card-img img img-fluid",src:e.image,alt:""})})})})}}}]);
+//# sourceMappingURL=9.4f067e30.chunk.js.map
--- static/js/9.ebefe526.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpe-commerce"]=this["webpackJsonpe-commerce"]||[]).push([[9],{42:function(e,c,s){"use strict";s.r(c);var t=s(0);c.default=function(e){return Object(t.jsx)("div",{className:"col-md-4 col-12",children:Object(t.jsx)("div",{className:"card bg-dark text-white",children:Object(t.jsx)("a",{href:"category.html",children:Object(t.jsx)("img",{className:"card-img img img-fluid",src:e.image,alt:""})})})})}}}]);
-//# sourceMappingURL=9.ebefe526.chunk.js.map
