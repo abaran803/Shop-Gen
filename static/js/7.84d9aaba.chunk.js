00c7b16c2a563d51d1fc9d5328288e61f84a0ec9
Ayush Baranwal <abaran803@gmail.com>
2022-05-22 13:34:33 +0530

    Updates

--- static/js/7.84d9aaba.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpe-commerce"]=this["webpackJsonpe-commerce"]||[]).push([[7],{50:function(e,c,s){"use strict";s.r(c);var t=s(0);c.default=function(e){return Object(t.jsx)("div",{className:"col-md-4 col-12 shadow p-3",children:Object(t.jsxs)("div",{className:"text-center",children:[Object(t.jsxs)("div",{className:"mb-4",children:[Object(t.jsx)("i",{className:e.item.iconClass}),Object(t.jsx)("div",{children:Object(t.jsx)("h3",{children:e.item.title})})]}),Object(t.jsx)("div",{children:e.item.desc})]})})}}}]);
-//# sourceMappingURL=7.84d9aaba.chunk.js.map
