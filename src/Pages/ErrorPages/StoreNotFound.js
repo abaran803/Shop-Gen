4107b47c0da045b84fbe2c1994b0fd8e0a526e16
abaran803 <webdevevolution@gmail.com>
2022-08-08 11:49:13 +0530

    Changed the link of Error Page image

--- src/Pages/ErrorPages/StoreNotFound.js
+++ src/Pages/ErrorPages/StoreNotFound.js
@@ -18,8 +18,8 @@ const StoreNotFound = () => {
   }
 
   return (
-    <div className='d-flex'>
-      <div className={` ${classes.errorText} vh-100 d-flex justify-content-center`}>
+    <div className='d-flex vh-100'>
+      <div className={` ${classes.errorText} h-100 d-flex justify-content-center`}>
         <div className='w-75 h-100 d-flex flex-column justify-content-center'>
           <div className='font-weight-lighter' style={{ fontSize: "2.2rem", lineHeight: "2.4rem" }}>May be, you have missed your shop id :(</div>
           <div className='my-3 text-secondary'>We are the platform to create the store for the businesses and provide unique store ID. If you are a customer of any store, please contact to the store owner for the correct ID</div>
@@ -27,12 +27,12 @@ const StoreNotFound = () => {
             <input className={classes.inputField} type="text" placeholder='Enter Shop ID' onChange={storeIdHandler} />
             <button className={`${classes.submitButton} h-100 bg bg-primary text-white rounded border-none`} onClick={() => gotoStore(enteredId)}>Go To Store</button>
           </div>
-          <button className={`${classes.submitButton} mx-0 my-2 px-3 py-2 rounded text-white`} style={{background: "rgba(80, 80, 80, 0.6)"}} disabled>Generate Your Store</button>
+          <button className={`${classes.submitButton} mx-0 my-2 px-3 py-2 rounded text-white`} style={{ background: "rgba(80, 80, 80, 0.6)" }} disabled>Generate Your Store</button>
           <button className={`${classes.submitButton} mx-0 my-1 px-3 py-2 bg bg-success rounded text-white`} onClick={() => gotoStore('62daadc9b528a5ec705e97c0')}>Demo Shop</button>
         </div>
       </div>
       <div className={`${classes.errorImage} w-50 vh-100 align-items-center justify-content-center`}>
-        <img className='w-100 h-100' src={logo} />
+      <lottie-player src="https://assets4.lottiefiles.com/packages/lf20_mkqmafk2.json" background="transparent" speed="1" style={{width: "100%", height: "100%"}} loop autoplay></lottie-player>
       </div>
     </div>
   )
