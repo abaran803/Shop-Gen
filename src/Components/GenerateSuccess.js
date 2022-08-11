fdb4dde4edd7e559ffb4849256ba7a4dcc0265de
abaran803 <webdevevolution@gmail.com>
2022-08-11 21:28:38 +0530

    Improved Shop Generator

--- src/Components/GenerateSuccess.js
+++ src/Components/GenerateSuccess.js
@@ -5,11 +5,20 @@ import { useLocation } from 'react-router-dom'
 const GenerateSuccess = () => {
     const location = useLocation();
     const history = useHistory();
-    if(!(location.state)) {
+    if (!(location.state)) {
         history.replace('/generate');
     }
     return (
-        <div>GenerateSuccess</div>
+        <div>{location.state && (
+            <div className='d-flex align-items-center justify-content-center vh-100'>
+                <div className='d-flex flex-column px-5 py-4 rounded bg bg-light border' style={{width: "500px", maxWidth: "90vw"}}>
+                <div>Brand Name&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp; {location.state.brandName}</div>
+                <div>Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp; {location.state.email}</div>
+                <div>Store ID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp; {location.state.storeId}</div>
+                <div>URL&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp; <a href={'https://abaran803.github.io/E-Commerce/#/' + location.state.storeId + '/home'} target="_blank">Click Here</a></div>
+                </div>
+            </div>
+        )}</div>
     )
 }
 
--- src/Components/ShopGenerator.js
+++ src/Components/ShopGenerator.js
@@ -1,43 +1,87 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import Loader from './Loader';
 
 const ShopGenerator = () => {
 
-    const history = useHistory();
-    const [storeData, setStoreData] = useState();
+  const history = useHistory();
+  const [storeData, setStoreData] = useState();
+  const [loading, setLoading] = useState();
+  const [selectedMethod, setSelectedMethod] = useState(0);
 
-    const handleChange = (e) => {
-        setStoreData(e.target.value);
-    }
+  const handleChange = (e) => {
+    setStoreData(e.target.value);
+  }
 
-    const handleSubmit = async (e) => {
-        const URL = 'http://localhost:8080/generateStore';
-        const data = JSON.parse(storeData);
-        e.preventDefault();
-        const response = await fetch(URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-        const jsonResponse = await response.json();
-        console.log(jsonResponse);
-        history.replace({
-            pathname: '/generateSuccess',
-            state: {
-                storeAdded: true
-            }
-        });
-    }
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    const URL = `${process.env.REACT_APP_BACKEND_URL}/generateStore`;
+    const data = JSON.parse(storeData);
+    const { brandName, email } = data;
+    e.preventDefault();
+    const response = await fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const jsonResponse = await response.json();
+    history.replace({
+      pathname: '/generateSuccess',
+      state: {
+        storeAdded: true,
+        brandName,
+        email,
+        storeId: jsonResponse.storeId
+      }
+    });
+  }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <textarea cols="30" rows="10" onChange={handleChange} value={storeData}></textarea>
-            <button type="submit">Submit</button>
-        </form>
-    )
+  const inputDataMethods = [
+    'JSON Data',
+    'JSON File',
+    'Fill Form'
+  ]
+
+  const methodMapping = [
+    jsonData(handleSubmit, handleChange, storeData),
+    jsonFile(handleSubmit, handleChange, storeData),
+    fillForm(handleSubmit, handleChange, storeData)
+  ]
+
+  if (loading) {
+    return <Loader />
+  }
+
+  return (
+    <div className='container'>
+      <ul className='d-flex w-100 justify-content-between mt-3' style={{ listStyleType: "none" }}>
+        {inputDataMethods.map((item, id) => (
+          <li key={id}>
+            <button type="button" className={`btn btn${!(selectedMethod === id) ? '-outline' : ''}-primary shadow-none`} onClick={() => setSelectedMethod(id)} key={id}>{item}</button>
+          </li>
+        ))}
+      </ul>
+      {methodMapping[selectedMethod === undefined ? 0 : selectedMethod]}
+    </div>
+  )
 }
+
+const jsonData = (handleSubmit, handleChange, storeData) => (
+  <form onSubmit={handleSubmit}>
+    <textarea cols="30" rows="10" onChange={handleChange} value={storeData}></textarea>
+    <button type="submit">Submit</button>
+  </form>
+)
+
+const jsonFile = (handleSubmit, handleChange, storeData) => (
+  <div>Hello</div>
+)
+
+const fillForm = (handleSubmit, handleChange, storeData) => (
+  <div>World</div>
+)
 
 export default ShopGenerator
